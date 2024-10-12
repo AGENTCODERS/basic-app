5e06aac59a1d8a418cfa316c2cf9156e0c6400ad
AGENTCODERS <159488623+AGENTCODERS@users.noreply.github.com>
2024-10-13 03:29:36 +0530

    get product,add prod,update product implemented

--- src/main/java/com/agentgroups/basicapp/controller/ProductController.java
+++ src/main/java/com/agentgroups/basicapp/controller/ProductController.java
@@ -1,11 +1,10 @@
 package com.agentgroups.basicapp.controller;
-
 import com.agentgroups.basicapp.entities.Product;
 import com.agentgroups.basicapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -14,8 +13,57 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @RequestMapping("/products")
-    public List<Product> getProducts(){
+    @GetMapping("/products")
+    public List<Product> getProducts() {
         return productService.getProducts();
     }
+
+    @GetMapping("/getproduct/{prodId}")
+    public Product getProductById(@PathVariable int prodId) {
+        return productService.getProductById(prodId);
+    }
+
+    @PostMapping("/addproduct")
+    public void addProduct(@Valid @RequestBody Product prod) {  // Updated method
+        productService.addProduct(prod);
+    }
+
+    @PutMapping("/updateproduct")
+    public void updateProduct(@RequestBody Product prod){
+        productService.updateProduct(prod);
+    }
 }
+
+
+
+
+//package com.agentgroups.basicapp.controller;
+//
+//import com.agentgroups.basicapp.entities.Product;
+//import com.agentgroups.basicapp.service.ProductService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//public class ProductController {
+//
+//    @Autowired
+//    ProductService productService;
+//
+//    @GetMapping("/products")
+//    public List<Product> getProducts(){
+//        return productService.getProducts();
+//    }
+//
+//    @GetMapping("/getproduct/{prodId}")
+//    public Product getProductById(@PathVariable int prodId){
+//        return productService.getProductById(prodId);
+//    }
+//
+//    @PostMapping("/addproduct")
+//    public void addProduct(Product prod){
+//        productService.addProduct(prod);
+//    }
+//}
